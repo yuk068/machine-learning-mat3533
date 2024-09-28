3557e569136a6618c476ec2ddcc5bf1b0bd010dd
yuk068 <duy682005zac@gmail.com>
2024-09-28 16:08:04 +0700

    LogR assignment unfinished

---
+++ Jupyter/logr-assignment/logr_utils.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+def sigmoid(s):
+    s = np.clip(s, -100, 100)
+    return 1 / (1 + np.exp(-s))
+
+def logistic_sigmoid_regression(X, y, w_init, eta, tol = 1e-4, max_count = 10000):
+    # method to calculate model logistic regression by Stochastic Gradient Descent method
+    # eta: learning rate; tol: tolerance; max_count: maximum iterates
+    w = [w_init]
+    it = 0
+    N = X.shape[1]
+    d = X.shape[0]
+    count = 0
+    check_w_after = 20
+    # loop of stochastic gradient descent
+    while count < max_count:
+    # shuffle the order of data (for stochastic gradient descent).
+    # and put into mix_id
+        mix_id = np.random.permutation(N)
+        for i in mix_id:
+            xi = X[:, i].reshape(d, 1)
+            yi = y[i]
+            zi = sigmoid(np.dot(w[-1].T, xi))
+            w_new = w[-1] + eta*(yi - zi)*xi
+            count += 1
+            # stopping criteria
+            if count%check_w_after == 0:
+                if np.linalg.norm(w_new - w[-check_w_after]) < tol:
+                    return w
+            w.append(w_new)
+    return w
