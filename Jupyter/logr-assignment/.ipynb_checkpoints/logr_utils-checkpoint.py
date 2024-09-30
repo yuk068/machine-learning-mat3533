9f11566ca479ddfeda9a3f29d18b617c530972e9
yuk068 <duy682005zac@gmail.com>
2024-09-30 12:22:07 +0700

    From laptop

--- Jupyter/logr-assignment/.ipynb_checkpoints/logr_utils-checkpoint.py
+++ Jupyter/logr-assignment/.ipynb_checkpoints/logr_utils-checkpoint.py
@@ -1,9 +1,11 @@
 import numpy as np
 
 def sigmoid(s):
+    s = np.clip(s, -100, 100)
     return 1 / (1 + np.exp(-s))
 
-def logistic_sigmoid_regression(X, y, w_init, eta, tol = 1e-4, max_count = 10000):
+def logistic_sigmoid_regression(X, y, w_init, eta, tol = 1e-4, max_count = 10000, epochs = 20):
+    print(epochs)
     # method to calculate model logistic regression by Stochastic Gradient Descent method
     # eta: learning rate; tol: tolerance; max_count: maximum iterates
     w = [w_init]
@@ -11,7 +13,7 @@ def logistic_sigmoid_regression(X, y, w_init, eta, tol = 1e-4, max_count = 10000
     N = X.shape[1]
     d = X.shape[0]
     count = 0
-    check_w_after = 20
+    check_w_after = epochs
     # loop of stochastic gradient descent
     while count < max_count:
     # shuffle the order of data (for stochastic gradient descent).
@@ -29,3 +31,8 @@ def logistic_sigmoid_regression(X, y, w_init, eta, tol = 1e-4, max_count = 10000
                     return w
             w.append(w_new)
     return w
+
+def min_max_normalize(X):
+    X_min = X.min(axis=0)
+    X_max = X.max(axis=0)
+    return (X - X_min) / (X_max - X_min)
--- Jupyter/logr-assignment/logr_utils.py
+++ Jupyter/logr-assignment/logr_utils.py
@@ -4,7 +4,7 @@ def sigmoid(s):
     s = np.clip(s, -100, 100)
     return 1 / (1 + np.exp(-s))
 
-def logistic_sigmoid_regression(X, y, w_init, eta, tol = 1e-4, max_count = 10000):
+def logistic_sigmoid_regression(X, y, w_init, eta, epochs, tol = 1e-4, max_count = 10000):
     # method to calculate model logistic regression by Stochastic Gradient Descent method
     # eta: learning rate; tol: tolerance; max_count: maximum iterates
     w = [w_init]
@@ -12,7 +12,7 @@ def logistic_sigmoid_regression(X, y, w_init, eta, tol = 1e-4, max_count = 10000
     N = X.shape[1]
     d = X.shape[0]
     count = 0
-    check_w_after = 20
+    check_w_after = epochs
     # loop of stochastic gradient descent
     while count < max_count:
     # shuffle the order of data (for stochastic gradient descent).
